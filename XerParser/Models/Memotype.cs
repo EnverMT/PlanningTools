5cf2e7dfae01ba342e0dd2c040f94949d59b50ce
EnverMT <anvarmtg@gmail.com>
2022-09-23 16:56:03 +0500

    v0.1

--- XerParser/Models/Memotype.cs
+++ XerParser/Models/Memotype.cs
@@ -2,8 +2,8 @@
 {
     public class Memotype
     {
-        public string? memo_type_id { get; set; }
-        public string? seq_num { get; set; }
+        public int memo_type_id { get; set; }
+        public int seq_num { get; set; }
         public string? proj_flag { get; set; }
         public string? wbs_flag { get; set; }
         public string? task_flag { get; set; }
