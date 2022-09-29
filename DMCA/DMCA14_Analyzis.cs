57f12956430882c693da618b6c77876c3483883b
EnverMT <anvarmtg@gmail.com>
2022-09-29 15:24:48 +0500

    Plan of features

--- DMCA/DMCA14_Analyzis.cs
+++ DMCA/DMCA14_Analyzis.cs
@@ -2,6 +2,22 @@
 {
     public class DMCA14_Analyzis
     {
+        /* To do list:
+         * [ ] No Sucesors count
+         * [ ] No Predecessors count
+         * [ ] Lags
+         * [ ] Leads
+         * [ ] Relations
+         * [ ] Constrains
+         * [ ] Large Total float
+         * [ ] Negative Total Float
+         * [ ] Durations
+         * [ ] Check for invalid Dates (no actual dates beyond Data Date)
+         * [ ] Resource assignments
+         * [ ] Slippage from target
+         * [ ] Critical path test (calculated as cirical path length + total float / critical path length)
+         * */
+
         public readonly int ActivitiesCount;
         public readonly int RelationshipsCount;
         public DMCA14_Analyzis(XerParser.Parse parse)
