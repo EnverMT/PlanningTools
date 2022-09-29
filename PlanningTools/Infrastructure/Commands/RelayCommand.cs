68e3b7c099f4a4925d889f7aabcc0530e4af18b8
EnverMT <anvarmtg@gmail.com>
2022-09-29 14:39:34 +0500

    cleaned unused tabs

--- PlanningTools/Infrastructure/Commands/RelayCommand.cs
+++ PlanningTools/Infrastructure/Commands/RelayCommand.cs
@@ -8,14 +8,14 @@ namespace XerParser.Infrastructure.Commands
         private readonly Action<object> _Execute;
         private readonly Func<object, bool> _CanExecute;
 
-        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecute)
         {
             _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
             _CanExecute = CanExecute;
         }
 
-        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter!) ?? true;
 
-        public override void Execute(object parameter) => _Execute(parameter);
+        public override void Execute(object? parameter) => _Execute(parameter!);
     }
 }
