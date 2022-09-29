75da7ad54e0dc5b3b34ea01ff5f7cb631508230e
EnverMT <anvarmtg@gmail.com>
2022-09-29 13:41:43 +0500

    cleaned

--- XerParser/Models/Tasks.cs
+++ XerParser/Models/Tasks.cs
@@ -6,8 +6,7 @@ namespace XerParser.Models
     public class TASKS : BaseModel<XerParser.Models.Classes.TASK>
     {
         public int Count()
-        {
-            List<TASKRSRC> a = list.FirstOrDefault().SearchResources(_parseContex).Resources;
+        {            
             return list.Count;
         }
     }
