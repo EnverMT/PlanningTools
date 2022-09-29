32be596c284f64efab6798d97d20cfbeeab1306b
EnverMT <anvarmtg@gmail.com>
2022-09-29 13:42:01 +0500

    cleaned

--- XerParser/Models/Tasks.cs
+++ XerParser/Models/Tasks.cs
@@ -1,12 +1,11 @@
 ﻿using XerParser.Models.Base;
-using XerParser.Models.Classes;
 
 namespace XerParser.Models
 {
     public class TASKS : BaseModel<XerParser.Models.Classes.TASK>
     {
         public int Count()
-        {            
+        {
             return list.Count;
         }
     }
