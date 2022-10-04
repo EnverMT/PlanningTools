d3b1acc09b6514067d263009d7c61473bedb3941
Timur <txiao57252@gmail.com>
2022-10-04 10:25:53 +0500

    Account data types corrected

--- DatabaseManager/Model/Account.cs
+++ DatabaseManager/Model/Account.cs
@@ -8,11 +8,11 @@
         public string? AcctName { get; set; }
         public string? AcctShortName { get; set; }
         public byte[]? AcctDescr { get; set; }
-        public byte[]? CreateDate { get; set; }
+        public DateTime? CreateDate { get; set; }
         public string? CreateUser { get; set; }
-        public byte[]? UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
         public string? UpdateUser { get; set; }
         public long? DeleteSessionId { get; set; }
-        public byte[]? DeleteDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
     }
 }
