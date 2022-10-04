ad14d717180ac52435115f1aed350747f6f6d840
Timur <txiao57252@gmail.com>
2022-10-04 16:54:24 +0500

    Corrected types DateTime?

--- DatabaseManager/Model/Rsrcrate.cs
+++ DatabaseManager/Model/Rsrcrate.cs
@@ -6,17 +6,17 @@
         public long? RsrcId { get; set; }
         public double? MaxQtyPerHr { get; set; }
         public double? CostPerQty { get; set; }
-        public byte[]? StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
         public long? ShiftPeriodId { get; set; }
         public double? CostPerQty2 { get; set; }
         public double? CostPerQty3 { get; set; }
         public double? CostPerQty4 { get; set; }
         public double? CostPerQty5 { get; set; }
-        public byte[]? CreateDate { get; set; }
+        public DateTime? CreateDate { get; set; }
         public string? CreateUser { get; set; }
-        public byte[]? UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
         public string? UpdateUser { get; set; }
         public long? DeleteSessionId { get; set; }
-        public byte[]? DeleteDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
     }
 }
