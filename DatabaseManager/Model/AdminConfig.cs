2d03338a8dac07ddbf464a9c2f54351f1f725b72
Timur <txiao57252@gmail.com>
2022-10-04 11:58:56 +0500

    Change types in Acvtype, AdminConfig, Asgnmntacat

--- DatabaseManager/Model/AdminConfig.cs
+++ DatabaseManager/Model/AdminConfig.cs
@@ -5,14 +5,14 @@
         public string ConfigName { get; set; } = null!;
         public string? ConfigType { get; set; }
         public string? FactoryVersion { get; set; }
-        public byte[]? LastChangeDate { get; set; }
+        public DateTime? LastChangeDate { get; set; }
         public string? ConfigValue { get; set; }
         public byte[]? ConfigData { get; set; }
-        public byte[]? CreateDate { get; set; }
+        public DateTime? CreateDate { get; set; }
         public string? CreateUser { get; set; }
-        public byte[]? UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
         public string? UpdateUser { get; set; }
         public long? DeleteSessionId { get; set; }
-        public byte[]? DeleteDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
     }
 }
